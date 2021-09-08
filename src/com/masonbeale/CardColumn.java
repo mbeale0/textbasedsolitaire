fc0ee9f9cb11c4ad52bd02573c244f97f0bb17f8
Mason Beale <mbeale@cedarville.edu>
2021-09-07 21:00:58 -0400

    removed old 'getStack() method'

--- src/com/masonbeale/CardColumn.java
+++ src/com/masonbeale/CardColumn.java
@@ -9,29 +9,6 @@ public class CardColumn {
         cardColumn.add(card);
     }
 
-    public Card[] getStack(int numOfCards){
-        Card[] cards = new Card[numOfCards];
-
-        int cardIndex = 0;
-        int colIndexSize = cardColumn.size() - 1;
-        if(numOfCards == 1){
-            cards[cardIndex] = cardColumn.get(colIndexSize);
-            cardColumn.remove(colIndexSize);
-        }
-        else{
-            int timesToLoop = colIndexSize -numOfCards;
-            for(int i = colIndexSize; i > timesToLoop; i--){
-                changeCards(cards, cardIndex, i);
-            }
-        }
-        if(cardColumn.size() != 0){
-            colIndexSize = cardColumn.size()-1;
-            cardColumn.get(colIndexSize).setFlipped(true);
-        }
-
-        return cards;
-    }
-
     private void changeCards(Card[] cards, int cardIndex, int i) {
         cards[cardIndex] = cardColumn.get(i);
         cardColumn.remove(i);
