5a01f524daa08bbf48996e23b2458786be9d76b6
Mason Beale <mbeale@cedarville.edu>
2021-09-08 20:24:42 -0400

    added card colors

--- src/com/masonbeale/Card.java
+++ src/com/masonbeale/Card.java
@@ -1,10 +1,15 @@
 package com.masonbeale;
 
+import java.awt.*;
+
 public class Card {
     private final int cardNumber;
     private final Suit cardSuit;
     private boolean isFlipped = false;
     private final Color cardColor;
+    private static final String ANSI_RESET = "\u001b[0m";
+    private static final String ANSI_BLACK = "\u001b[30m";
+    private static final String ANSI_RED = "\u001b[31m";
 
     public enum Color{
         red,
@@ -48,17 +53,17 @@ public class Card {
     public String toString() {
         String printedCardSuit;
         if(cardSuit == Suit.Diamonds){
-            printedCardSuit = "♦";
+            printedCardSuit = ANSI_RED + "♦";
         }
         else if(cardSuit == Suit.Clubs){
-            printedCardSuit = "♣";
+            printedCardSuit = ANSI_BLACK + "♣";
         }
         else if(cardSuit == Suit.Hearts){
-            printedCardSuit = "♥";
+            printedCardSuit = ANSI_RED + "♥";
         }
         else{
-            printedCardSuit = "♠";
+            printedCardSuit =  ANSI_BLACK + "♠";
         }
-        return cardNumber + printedCardSuit;
+        return cardNumber + printedCardSuit + ANSI_RESET;
     }
 }
